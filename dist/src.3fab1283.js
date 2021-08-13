4b27d14ae6e597b179c04cea45b6d097cf993c76
czc222 <1263291450@qq.com>
2021-08-13 18:27:58 +0800

    版本1.0

---
+++ dist/src.3fab1283.js
@@ -0,0 +1,307 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({"index.js":[function(require,module,exports) {
+var html = document.querySelector("#html"); /* 通过css选择器找到这个html */
+var style = document.querySelector("#style");
+var string = "\n/* \u4F60\u597D\uFF0C\u6211\u662F\u4E00\u540D\u524D\u7AEF\u65B0\u4EBA\n\u63A5\u4E0B\u6765\u6211\u8981\u5F04\u4E00\u4E2A\u9634\u9633\u56FE\n\u5148\u753B\u4E00\u4E2A\u5706\n*/\n#circle {\nwidth:300px;\nheight:300px;\nbox-shadow:0 0 3px rgba(0,0,0,.5);\nborder-radius:50%;\nleft:50%;\n}\n/*\u63A5\u4E0B\u6765\u6211\u8BA9\u8BA9\u5706\u53D8\u6210\u9634\u9633\u4E24\u8272\n *\u7136\u540E\u8BA9\u8FD9\u4E2A\u5706\u53D8\u6210\u9634\u9633\u4EA4\u6C47\n *\u6700\u540E\u5728\u91CC\u9762\u7ED8\u5236\u9634\u9633\u773C\n */\n#circle {\n    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);\n}\n#circle::before {\n    content:'';\n    display:block;\n    width:150px;\n    height:150px;\n    background:black;\n    border-radius:50%;\n    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);\n}\n#circle::after {\n    content:'';\n    display:block;\n    width:150px;\n    height:150px;\n    background:white;\n    border-radius:50%;\n    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);\n}\n\n"; //加上注释让文字不会影响到body的样式
+var string2 = ''; //让string2等于一个空字符串
+var n = 0; //初始化n的值，下标为0
+
+var word = function word() {
+
+    if (string[n] === '\n') {
+        //如果string字符串等于回车
+
+        string2 = string2 + "<br>"; //那就变成css中的br，让html显示回车效果
+    } else if (string[n] === ' ') {
+        string2 += "&nbsp;";
+    } else {
+        string2 = string2 + string[n]; //否则就让string2等于string
+    } /*简化版 string2+=string[n] ==='\n' ? '<br>':string[n] */
+    setTimeout(function () {
+        n = n + 1; //让n递增
+        html.innerHTML = string2; //html显示string2字符串
+        style.innerHTML = string.substring(0, n);
+        window.scrollTo(0, 999999); //控制滚动条
+        html.scrollTo(0, 999999);
+        if (n < string.length) {
+            word(); //循环执行
+        }
+    }, 10);
+};
+word();
+},{}],"D:\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '62812' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      console.clear();
+
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},["D:\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js","index.js"], null)
+//# sourceMappingURL=/src.3fab1283.map
---
+++ src/index.js
@@ -0,0 +1,72 @@
+let html = document.querySelector("#html");/* 通过css选择器找到这个html */
+let style =document.querySelector("#style")
+let string =`
+/* 你好，我是一名前端新人
+接下来我要弄一个阴阳图
+先画一个圆
+*/
+#circle {
+width:300px;
+height:300px;
+box-shadow:0 0 3px rgba(0,0,0,.5);
+border-radius:50%;
+left:50%;
+}
+/*接下来我让让圆变成阴阳两色
+ *然后让这个圆变成阴阳交汇
+ *最后在里面绘制阴阳眼
+ */
+#circle {
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+#circle::before {
+    content:'';
+    display:block;
+    width:150px;
+    height:150px;
+    background:black;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+}
+#circle::after {
+    content:'';
+    display:block;
+    width:150px;
+    height:150px;
+    background:white;
+    border-radius:50%;
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+}
+
+`//加上注释让文字不会影响到body的样式
+let string2=''//让string2等于一个空字符串
+let n = 0 //初始化n的值，下标为0
+
+let word = ()=>{
+    
+    
+    if(string[n]==='\n'){//如果string字符串等于回车
+        
+          string2=string2+"<br>"//那就变成css中的br，让html显示回车效果
+          
+    } else if(string[n]===' '){
+        string2+="&nbsp;"
+        }else
+        {
+        string2=string2+string[n]//否则就让string2等于string
+
+    } /*简化版 string2+=string[n] ==='\n' ? '<br>':string[n] */
+    setTimeout(()=>{
+        n=n+1 ;//让n递增
+        html.innerHTML = string2;//html显示string2字符串
+        style.innerHTML = string.substring(0,n)
+        window.scrollTo(0,999999);//控制滚动条
+        html.scrollTo(0,999999)
+        if(n <string.length){
+            word() ;//循环执行
+        }
+        
+    },10)
+    
+}
+word()
